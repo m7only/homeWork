d3487fbbbb4fd7b01467df1f4d5ed0cb807eb523
Only <ongranly@ngs.ru>
2022-12-20 15:37:55 +0700

    Домашнее задание Методы объектов.

--- src/pro/sky/java/course1/lesson1/Book.java
+++ src/pro/sky/java/course1/lesson1/Book.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.lesson1;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
@@ -26,4 +28,23 @@ public class Book {
     public void setPublishedYear(int publishedYear) {
         this.publishedYear = publishedYear;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%s. %s, %d", author, title, publishedYear);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishedYear == book.publishedYear && title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publishedYear);
+    }
 }
+
--- src/pro/sky/java/course1/lesson1/Main.java
+++ src/pro/sky/java/course1/lesson1/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Author authorOfSteel = new Author("Николай", "Островский");
         Author authorOfLand = new Author("Михаил", "Шолохов");
+        Author authorOfLand2 = new Author("Михаил", "Шолохов");
         Book steel = new Book("Как закалялась сталь", authorOfSteel, 1932);
         Book land = new Book("Поднятая целина", authorOfLand, 1932);
         land.setPublishedYear(1959);
