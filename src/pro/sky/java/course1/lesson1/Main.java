b6ca49d2cb87fc5ccf457a9cd9bba37909718f35
Only <ongranly@ngs.ru>
2022-12-20 15:41:02 +0700

    Домашнее задание Методы объектов.

--- src/pro/sky/java/course1/lesson1/Main.java
+++ src/pro/sky/java/course1/lesson1/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Author authorOfSteel = new Author("Николай", "Островский");
         Author authorOfLand = new Author("Михаил", "Шолохов");
-        Author authorOfLand2 = new Author("Михаил", "Шолохов");
         Book steel = new Book("Как закалялась сталь", authorOfSteel, 1932);
         Book land = new Book("Поднятая целина", authorOfLand, 1932);
         land.setPublishedYear(1959);
+
+        System.out.println(steel);
+        System.out.println(land);
+
+        Book land2 = new Book("Поднятая целина", authorOfLand, 1959);
+        System.out.println(land.equals(land2));
     }
 }
